1910b716ae2cb7b9ac1547088424bb75ecd49aa7
Winnie <759689674@qq.com>
2018-09-18 21:50:29 +0800

    918-21点46分

---
+++ src/components/select/city.vue
@@ -0,0 +1,32 @@
+<template>
+  <Select size="small" clearable style="width: 150px" >
+    <Option v-for="(item,index) in cityData" :value="item.name" :key="item.index">{{item.name}}</Option>
+  </Select>
+</template>
+
+<script>
+  import http from '@/http.js'
+    export default {
+        name: "city",
+      data(){
+        return{
+          cityData:[],
+        }
+      },
+      mounted(){
+        this.init()
+      },
+      methods:{
+        init(){
+          http.get('api/get_all_city',{}).then(resp=>{
+            this.cityData = resp.data.hits;
+            console.log('11111111111111:::',this.cityData)
+          })
+        },
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
