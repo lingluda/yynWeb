1559cdd4a5e4e65f3e403386fa658d40fed91b12
HMQ <327756291@qq.com>
2018-09-21 16:53:09 +0800

    游客画像

--- src/page/touristimg.vue
+++ src/page/touristimg.vue
@@ -16,12 +16,12 @@
       <DatePicker type="date" v-model="picDate" placeholder="自选时间" style="width: 120px"></DatePicker>
       <Row :gutter="16" style="margin-top: 20px">
         <Col span="6">
-            <div style="display: flex;border: 1px solid #dcdee2;height: 320px;width: 100%">
-            <div style="width: 10%;height: 320px;display: flex">
+            <div style="display: flex;border: 1px solid #dcdee2;height: 400px;width: 100%">
+            <div style="width: 10%;height: 400px;display: flex">
               <Icon type="ios-woman" size="32" color="#ff50fe" style="display: flex;align-items: center;margin-left: 60%"/>
             </div>
-            <div id="sex" style="width: 80%;height: 320px;"></div>
-            <div style="width: 10%;height: 320px;display: flex">
+            <div id="sex" style="width: 80%;height: 400px;"></div>
+            <div style="width: 10%;height: 400px;display: flex">
               <Icon type="ios-man" size="32" color="#1ea9ff" style="display: flex;align-items: center;"/>
             </div>
             </div>
@@ -37,7 +37,7 @@
         </Col>
       </Row>
 
-      <Row :gutter="16" style="margin-top: 20px">
+      <Row :gutter="16" style="margin-top: 23px">
         <Col span="12">
           <div id="age" class="tousimg_zb"></div>
         </Col>
@@ -46,8 +46,7 @@
         </Col>
 
       </Row>
-      <Row :gutter="16" style="margin-top: 20px;margin-bottom: 40px">
-
+      <Row :gutter="16" style="margin-top: 23px;">
         <Col span="12">
           <div id="city" class="tousimg_zb"></div>
         </Col>
@@ -72,87 +71,78 @@
       <Select style="width: 120px" placeholder="清选择">
         <Option>昆明</Option>
       </Select>
-      <Row :gutter="16" style="margin-top: 20px;margin-bottom: 40px">
+      <Row :gutter="16" style="margin-top: 20px;margin-bottom: 10px">
         <Col span="10">
           <div>
-            <Tabs v-model="tabname" style="border-top: 1px solid #dcdee2;border-left: 1px solid #dcdee2;border-right: 1px solid #dcdee2">
+            <Tabs v-model="tabname" style="border-top: 1px solid #dcdee2;border-left: 1px solid #dcdee2;height: 557px">
               <TabPane label="迁入" name="in">
-                <Table  height="449" :columns="columns1" :data="data1"></Table>
+                <Table  height="550" :columns="columns1" :data="data1"></Table>
               </TabPane>
               <TabPane label="迁出" name="out">
-                <Table  height="449" :columns="columns1" :data="data1"></Table>
+                <Table  height="550" :columns="columns1" :data="data1"></Table>
               </TabPane>
             </Tabs>
           </div>
         </Col>
         <Col span="14">
-          <div id="moveMap" style="border: 1px solid #dcdee2;height: 500px"></div>
+          <div id="moveMap" style="border: 1px solid #dcdee2;height: 557px"></div>
         </Col>
       </Row>
     </card>
     <card style="margin-top: 20px">
-      <div style="margin-bottom: 20px">
-        <span style="font-weight: bold;color: #000000">消费维度分析</span>
+      <div class="card_title">
+        <span style="font-weight: bold;color: #000000">一机游用户消费维度分析</span>
         <Tooltip content="Hereisthe111111111111111prompt text" placement="right" max-width="200"><Icon size="19" style="margin-bottom: 1px" type="ios-help-circle-outline" />
         </Tooltip>
       </div>
-    <RadioGroup type="button">
-      <Radio label="all">当日</Radio>
-      <Radio label="default">当月</Radio>
-    </RadioGroup>
-      <DatePicker type="date" placeholder="自选时间" style="width: 120px"></DatePicker>
-      <Row :gutter="16" style="margin-top: 20px;margin-bottom: 40px">
-        <Col span="12">
-          <Row style="height: 363.67px;border: 1px solid #dcdee2;padding: 20px;">
-            <div style="height: 50%;border-bottom: 1px solid #dcdee2">
-              <DatePicker v-model="picDate3" type="date" placeholder="Select date" style="width: 150px;float: right"></DatePicker>
-
-              <Col span="4">
-                <img src="../assets/imgs/cash1.png" style="margin-top: 20px"/>
+      <div style="height:350px">
+           <Row :gutter="16" style="padding: 0 30px 0 8px;display:flex;height:100%">
+              <Col span="10" style="border: 1px solid #dcdee2;margin-right:20px;height:100%;padding:0 30px">
+                <div style="padding:20px 0;height:50%;border-bottom:1px solid #dcdee2">
+                  <DatePicker v-model="picDate3" type="date" placeholder="Select date" style="width: 150px;float: right"></DatePicker>
+                  <div style="margin-top:40px;display:flex">
+                      <img src="../assets/imgs/cash1.png" style="margin-right:20px;width:60px;height:60px"/>
+                      <div>
+                         <div style="color:#000;font-size:16px">用户平均消费金额</div>
+                          <div style="color:#006eff"><span style="font-size: 32px;font-weight:600">{{vagprice}}</span>元</div>
+                      </div>
+                  </div>
+                </div>
+                <div style="padding:20px 0;height:50%;">
+                  <div style="margin-top:25px;display:flex">
+                      <img src="../assets/imgs/cash2.png" style="margin-right:20px;width:60px;height:60px"/>
+                      <div>
+                         <div style="color:#000;font-size:16px">中位点用户消费金额</div>
+                          <div style="color:#006eff"><span style="font-size: 32px;font-weight:600">{{middle}}</span>元</div>
+                      </div>
+                  </div>
+                </div>
               </Col>
-              <Col span="20">
-                <p style="font-weight: bold">用户平均消费金额</p>
-                <span style="font-size: 32px">{{vagprice}}</span>元
+              <Col span="14" style="border: 1px solid #dcdee2;height:100%">
+                <div style="padding-bottom: 20px;padding: 20px">
+                  <span style="font-weight: bold;color: #000000">热门路线TOP10</span>
+                  <Tooltip content="Hereisthe111111111111111prompt text" placement="right" max-width="200"><Icon size="19" style="margin-bottom: 1px" type="ios-help-circle-outline" />
+                  </Tooltip>
+                  <DatePicker v-model="picDate3" type="date" placeholder="Select date" style="width: 150px;float: right"></DatePicker>
+                </div>
+                <div id="cash" style="height:300px;width:100%"></div>
               </Col>
-            </div>
-            <div style="height: 50%;margin-top: 20px">
-              <Col span="4">
-                <img src="../assets/imgs/cash1.png"/>
-              </Col>
-              <Col span="20">
-                <p style="font-weight: bold">中位点用户消费金额</p>
-                <span style="font-size: 32px">{{middle}}</span>元
-              </Col>
-            </div>
-          </Row>
-        <!--  <div style="height: 300px;border: 1px solid #dcdee2">
-            <div style="margin-left: 20px;margin-top: 20px">
-            <p style="font-weight: bold">用户平均消费金额</p>
-            <span style="font-size: 32px">{{vagprice}}</span>元
-            </div>
-            <Divider></Divider>
-            <div style="margin-left: 20px">
-            <p style="font-weight: bold">中位点用户消费金额</p>
-            <span style="font-size: 32px">{{middle}}</span>元
-            </div>
-          </div>-->
-        </Col>
-        <Col span="12" style="border: 1px solid #dcdee2;">
-          <div style="padding-bottom: 20px;padding: 20px">
-            <span style="font-weight: bold;color: #000000">热门路线TOP10</span>
-            <Tooltip content="Hereisthe111111111111111prompt text" placement="right" max-width="200"><Icon size="19" style="margin-bottom: 1px" type="ios-help-circle-outline" />
-            </Tooltip>
-            <DatePicker v-model="picDate3" type="date" placeholder="Select date" style="width: 150px;float: right"></DatePicker>
-          </div>
-          <div id="cash" style="height:300px;width:100%"></div>
-        </Col>
-      </Row>
+           </Row>
+      </div>
     </card>
     </div>
   </div>
 </template>
 <style lang="less" scoped>
-.tits{line-height: 60px;font-size: 16px;padding-left: 20px;font-weight: 600;color: #000;background: #fff;border-bottom: 1px solid #e2e4e6}
+.tits {
+  line-height: 60px;
+  font-size: 16px;
+  padding-left: 20px;
+  font-weight: 600;
+  color: #000;
+  background: #fff;
+  border-bottom: 1px solid #e2e4e6;
+}
 
 .ivu-table-wrapper {
   border: unset;
@@ -160,8 +150,25 @@
 div.ivu-table:after {
   background-color: unset !important;
 }
-.card_title{line-height:30px;height:40px;font-size:15px}
-.tousimg_zb{width: 100%;height: 400px;border: 1px solid #dcdee2;padding:0 30px 15px;}
+.card_title {
+  line-height: 30px;
+  height: 40px;
+  font-size: 15px;
+}
+.tousimg_zb {
+  width: 100%;
+  height: 400px;
+  border: 1px solid #dcdee2;
+  padding: 0 30px 15px;
+}
+.ivu-col-span-6,
+.ivu-col-span-12 {
+  padding-left: 8px !important;
+  padding-right: 15px !important;
+}
+.ivu-tabs-nav {
+  width: 100% !important;
+}
 </style>
 
 <script>
@@ -267,7 +274,8 @@ export default {
           textStyle: {
             fontSize: 14
           },
-          x: "0"
+          x: "15",
+          y: "15"
         },
         tooltip: {
           trigger: "item",
@@ -282,8 +290,8 @@ export default {
           x: "center",
           icon: "circle",
           data: ["女士", "男士"],
-          itemWidth:10,
-          itemHeight:12
+          itemWidth: 10,
+          itemHeight: 12
         },
         series: [
           {
@@ -324,7 +332,7 @@ export default {
             fontSize: 14
           },
           x: "15",
-          y:"15"
+          y: "15"
         },
         tooltip: {
           trigger: "item",
@@ -338,7 +346,10 @@ export default {
           bottom: 3,
           x: "center",
           icon: "circle",
-          data: this.ageDatax
+          data: this.ageDatax,
+          itemGap: 20,
+          itemWidth: 10,
+          itemHeight: 12
         },
         series: [
           {
@@ -392,7 +403,7 @@ export default {
             fontSize: 14
           },
           x: "15",
-          y:"15"
+          y: "15"
         },
         tooltip: {
           trigger: "item",
@@ -406,7 +417,9 @@ export default {
           bottom: 10,
           x: "center",
           icon: "circle",
-          data: this.eduDatax
+          data: this.eduDatax,
+          itemWidth: 10,
+          itemHeight: 12
         },
         series: [
           {
@@ -459,8 +472,8 @@ export default {
           textStyle: {
             fontSize: 14
           },
-         x: "15",
-          y:"15"
+          x: "15",
+          y: "15"
         },
         color: [
           "#006EFF",
@@ -488,7 +501,9 @@ export default {
           bottom: 10,
           x: "center",
           icon: "circle",
-          data: this.inDatax
+          data: this.inDatax,
+          itemWidth: 10,
+          itemHeight: 12
         },
         series: [
           {
@@ -579,11 +594,12 @@ export default {
       let age = this.$echarts.init(document.getElementById("age"));
       age.setOption({
         title: {
-          text: "有车情况(%)",
+          text: "有车情况",
           textStyle: {
-            fontSize: 14
+            fontSize: 16
           },
-          x: "center"
+          x: "0",
+          y: 20
         },
         tooltip: {
           trigger: "axis",
@@ -614,7 +630,7 @@ export default {
             name: "占比",
             type: "bar",
             data: this.carDatay,
-            barWidth: "50%",
+            barWidth: "15%",
             label: {
               normal: {
                 show: true,
@@ -630,11 +646,12 @@ export default {
       let edu = this.$echarts.init(document.getElementById("edu"));
       edu.setOption({
         title: {
-          text: "终端类型(%)",
+          text: "终端类型",
           textStyle: {
-            fontSize: 14
+            fontSize: 16
           },
-          x: "center"
+          x: "0",
+          y: 20
         },
         tooltip: {
           trigger: "axis",
@@ -665,7 +682,7 @@ export default {
             name: "占比",
             type: "bar",
             data: this.mobiley,
-            barWidth: "50%",
+            barWidth: "15%",
             label: {
               normal: {
                 show: true,
@@ -681,11 +698,12 @@ export default {
       let city = this.$echarts.init(document.getElementById("city"));
       city.setOption({
         title: {
-          text: "来源城市(%)",
+          text: "来源城市",
           textStyle: {
-            fontSize: 14
+            fontSize: 16
           },
-          x: "center"
+          x: "0",
+          y: 20
         },
         tooltip: {
           trigger: "axis",
@@ -716,7 +734,7 @@ export default {
             name: "占比",
             type: "bar",
             data: this.cityy,
-            barWidth: "50%",
+            barWidth: "15%",
             label: {
               normal: {
                 show: true,
@@ -732,11 +750,12 @@ export default {
       let pro = this.$echarts.init(document.getElementById("province"));
       pro.setOption({
         title: {
-          text: "来源省份(%)",
+          text: "来源省份",
           textStyle: {
-            fontSize: 14
+            fontSize: 16
           },
-          x: "center"
+          x: "0",
+          y: 20
         },
         tooltip: {
           trigger: "axis",
@@ -767,7 +786,7 @@ export default {
             name: "占比",
             type: "bar",
             data: this.provy,
-            barWidth: "50%",
+            barWidth: "15%",
             label: {
               normal: {
                 show: true,
